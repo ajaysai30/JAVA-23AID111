dc36b1950010d6d9b0a73ab2b66d9b8fe550f54d
Ajay <ajayb.tech29@gmail.com>
2026-04-09 23:33:53 +0530

    Add threading example with MyThread and MyRunnable

---
+++ HackerRank/Threads.java
@@ -0,0 +1,25 @@
+class MyThread extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Thread 1: " + i);
+        }
+    }
+}
+
+class MyRunnable implements Runnable {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Thread 2: " + i);
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        Thread t2 = new Thread(new MyRunnable());
+
+        t1.start();
+        t2.start();
+    }
+}
