fbfc11d6afa511344151d115a068b73348706bb7
Ajay <ajayb.tech29@gmail.com>
2026-04-18 16:29:00 +0530

    Add NullPointerException handling example

---
+++ Exception_Handling/NullPonterException.java
@@ -0,0 +1,11 @@
+class Main{
+    public static void main(String[] args){
+        String name=null;
+        try{
+            System.out.println(name.length());
+        }
+        catch(NullPointerException e){
+            System.out.println("null pointer error");
+        }
+    }
+}
