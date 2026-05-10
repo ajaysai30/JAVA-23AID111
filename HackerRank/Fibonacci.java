919407987f0bb2badc22d499a05a1274b8417105
Ajay <ajayb.tech29@gmail.com>
2026-05-10 20:52:00 +0530

    Create Fibonacci.java

---
+++ HackerRank/Fibonacci.java
@@ -0,0 +1,12 @@
+class Main{
+    public static void main(String[] args){
+        int a=0,b=1;
+        System.out.print(a+" "+b+" ");
+        for(int i=0;i<=10;i++){
+            int c=a+b;
+            System.out.print(" "+c);
+            a=b;
+            b=c;
+        }
+    }
+}
